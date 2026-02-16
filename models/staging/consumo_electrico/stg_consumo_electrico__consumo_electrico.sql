80b496169ff017430c047295862d7f1e5d9b2d6e
pabloJimenez910.alu@stemgranada.com <pabloJimenez910.alu@stemgranada.com>
2026-02-16 18:17:37 +0000

    Corregidos los nombres de las zonas en zone_name

--- models/staging/consumo_electrico/stg_consumo_electrico__consumo_electrico.sql
+++ models/staging/consumo_electrico/stg_consumo_electrico__consumo_electrico.sql
@@ -1,8 +1,12 @@
-with 
-
-src_consumo as 
-(
-    select * from {{ source("src_consumo_electrico", "consumo_zonas")}}
+with src_consumo as (
+    select *
+    from {{ source("src_consumo_electrico", "consumo_zonas") }}
 )
 
-select * from src_consumo
+select
+    CONSUMPTION_KWH,
+    TEMPERATURE,
+    TS,
+    ZONE_ID,
+    upper(replace(trim(ZONE_NAME), ' ', '_')) as ZONE_NAME
+from src_consumo
